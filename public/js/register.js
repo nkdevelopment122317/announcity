8bc5d68685a688a2f69245d4e7bb2739db72ebee
Nikhil Kolachalama <geomathnikhil903@gmail.com>
2018-01-01 21:22:39 -0500

    remove radio buttons; begin to add school search during registration

--- public/js/register.js
+++ public/js/register.js
@@ -1,29 +1,12 @@
 $(document).ready(function() {
-    addEvents();
+    // addEvents();
 });
 
 function addEvents() {
-    //for role radio buttons
-    $(".role-input").on("click", function() {
-        var newPlaceholder = "Enter " + $(this).val().toLowerCase() + " code";
-        $("#code-label").text(newPlaceholder);
-    });
-
     $(".material-input").on("focus", function() {
         $(this).parent().parent().css("box-shadow", "0px 0px 23px 0px rgba(0,0,0,0.5)");
         $(this).parent().parent().css("-moz-box-shadow", "0px 0px 23px 0px rgba(0,0,0,0.5)");
         $(this).parent().parent().css("-webkit-box-shadow", "0px 0px 23px 0px rgba(0,0,0,0.5)");
-    }).on("blur", function() {
-        $(this).parent().parent().css("box-shadow", "none");
-        $(this).parent().parent().css("-moz-box-shadow", "none");
-        $(this).parent().parent().css("-webkit-box-shadow", "none");
-    });
-
-    $(".role-input").on("focus", function() {
-        $(this).parent().parent().css("box-shadow", "0px 0px 23px 0px rgba(0,0,0,0.5)");
-        $(this).parent().parent().css("-moz-box-shadow", "0px 0px 23px 0px rgba(0,0,0,0.5)");
-        $(this).parent().parent().css("-webkit-box-shadow", "0px 0px 23px 0px rgba(0,0,0,0.5)");
-        $(this).prop("checked", true);
     }).on("blur", function() {
         $(this).parent().parent().css("box-shadow", "none");
         $(this).parent().parent().css("-moz-box-shadow", "none");
